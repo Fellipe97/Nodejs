f13d1f8e12070c1a39f8dabcc0940d3618b5bfae
Fellipe97 <lfalmeida97@hotmail.com>
2021-12-21 14:22:31 -0300

    projeto finalizado

--- B7Web - Nodejs/Modulo2-Projeto-Canil/src/controllers/pageController.ts
+++ B7Web - Nodejs/Modulo2-Projeto-Canil/src/controllers/pageController.ts
@@ -1,23 +1,60 @@
 import { Request, Response } from 'express';
 
+import { Pet } from '../models/pet'
+import { createMenuObject } from '../helpers/createMenuObject'
+
 
 export const home = (Req: Request, res: Response) => {
-    res.send('Próxima aula, módulo2: video 5!!');
-    //res.render('pages/page');
+    let list = Pet.getAll();
+
+    res.render('pages/page',{
+        menu: createMenuObject('all'),
+        banner: {
+            title: 'Todos os animais',
+            background: 'allanimals.jpg'
+        },
+        list
+    });
 };
 
 
 export const dogs = (Req: Request, res: Response) => {
-    //res.render('pages/dogs');
+    let list = Pet.getFromType('dog');
+
+    res.render('pages/page',{
+        menu: createMenuObject('dog'),
+        banner: {
+            title: 'Cachorros',
+            background: 'banner_dog.jpg'
+        },
+        list
+    });
 };
 
 
 export const cats = (Req: Request, res: Response) => {
-    //res.render('pages/cats');
+    let list = Pet.getFromType('cat');
+
+    res.render('pages/page',{
+        menu: createMenuObject('cat'),
+        banner: {
+            title: 'Gatos',
+            background: 'banner_cat.jpg'
+        },
+        list
+    });
 };
 
 
 export const fishes = (Req: Request, res: Response) => {
-    //res.render('pages/fishes');
-};
+    let list = Pet.getFromType('fish');
 
+    res.render('pages/page',{
+        menu: createMenuObject('fish'),
+        banner: {
+            title: 'Peixes',
+            background: 'banner_fish.jpg'
+        },
+        list
+    });
+};
