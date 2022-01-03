45ca7d11d9c46cff76da707a83d60f188f26add7
Fellipe97 <lfalmeida97@hotmail.com>
2022-01-03 08:43:33 -0300

    Projeto Base

---
+++ B7Web - Nodejs/Modulo5-Criacao-de-API/src/server.ts
@@ -0,0 +1,17 @@
+import express, { Request, Response } from 'express';
+import path from 'path';
+import dotenv from 'dotenv';
+
+dotenv.config();
+
+const server = express();
+
+server.use(express.static(path.join(__dirname, '../public')));
+server.use(express.urlencoded({extended: true}));
+
+server.use( (req: Request, res: Response)=> {
+    res.status(400);
+    res.json({erros: 'Endpoint não encontrado.'});
+});
+
+server.listen(process.env.PORT);
